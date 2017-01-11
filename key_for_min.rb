4ecb742d9dde0a217d120640f05270005e235eef
jason brown <j.grant.brown@gmail.com>
2017-01-11 14:05:20 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+  smallest_item= nil
+	champion_value = 100000
+	name_hash.each do |key, value|
+		if  value < champion_value
+			champion_value = value
+			smallest_item = key
+			end
+end
+ smallest_item
 end
